0ad787adf76cc476d04fbb962f7158f2f7fc4b7b
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-04-29 08:16:52 -0500

    some werk on login

---
+++ src/constants/routes.js
@@ -0,0 +1,5 @@
+export const DASHBOARD = '/';
+export const LOGIN = '/login';
+export const SIGN_UP = '/signip';
+export const PROFILE = '/p:username';
+export const NOT_FOUND = '/not-found';
---
+++ src/pages/login.js
@@ -0,0 +1,25 @@
+import { useHistory } from 'react-router-dom';
+import React, { useContext, useState, useEffect } from 'react';
+import FirebaseContext from '../context/firebase';
+
+export default function Login() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, serError] = useState('');
+  const isInvalid = password === '' || emailAddress === '';
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = 'Login - INstaclone';
+  }, []);
+
+  return (
+    <div className="container flex mx-auto mzx-w-screen-md items-cents h-screen">
+      <p>No idear</p>
+    </div>
+  );
+}
