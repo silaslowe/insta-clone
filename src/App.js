46ab38e5df5482355fe344ddf36e416976afcd5b
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-04-29 08:36:54 -0500

    hmmm

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
@@ -14,29 +13,6 @@ function App() {
         </Switch>
       </Suspense>
     </Router>
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> main
   );
 }
 
