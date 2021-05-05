adbc4d166fc9c5a2adeb56099a4ff453b06bd45e
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-05-04 19:28:01 -0500

    dashboard components set up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
+import Dashboard from './pages/dashboard';
 
 const Login = lazy(() => import('./pages/login'));
 const Signup = lazy(() => import('./pages/sign-up'));
@@ -13,8 +14,9 @@ function App() {
     <Router>
       <Suspense fallback={<p>loading...</p>}>
         <Switch>
-          <Route path={ROUTES.LOGIN} component={Login} exact/>
-          <Route path={ROUTES.SIGN_UP} component={Signup} exact/>
+          <Route path={ROUTES.LOGIN} component={Login} exact />
+          <Route path={ROUTES.SIGN_UP} component={Signup} exact />
+          <Route path={ROUTES.DASHBOARD} component={Dashboard} />
           <Route component={NotFound}/>
         </Switch>
       </Suspense> 
