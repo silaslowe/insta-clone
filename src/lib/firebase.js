5f6def02c24e0b5d057f1f7becfe0a4341105468
Silas Lowe <60553895+silaslowe@users.noreply.github.com>
2021-05-12 16:34:39 -0500

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,12 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyDXfx1pu2CpIZwXguh4RuovpUu3A9dXotU",
-  authDomain: "insta-clone-3.firebaseapp.com",
-  projectId: "insta-clone-3",
-  storageBucket: "insta-clone-3.appspot.com",
-  messagingSenderId: "519277781591",
-  appId: "1:519277781591:web:04b0c813886ba3683bb912"
+
 };
 
 const firebase = Firebase.initializeApp(config);
