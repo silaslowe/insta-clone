f37a3e2dee8180613f80b4436b8fc17385aa4386
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-05-17 11:56:00 -0500

    username and full name and image loading

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -7,14 +7,20 @@ const User = ({username, fullName}) =>
     !username || !fullName ? (
         <Skeleton count={1} height={61} />
     ) : (
-        <Link to={`/p/${username}`} className="grid">
-            <p>{username}</p>
+        <Link to={`/p/${username}`} className="grid grid-cols-4 gap-4 mb-6 items center ">
+            <div className="flex items-center justify-between col-span-1">
+                <img src={`/images/avatars/${username}.jpg`} alt="" className="rounded-full w-16 flex mr-3" />
+            </div>
+            <div className="col-span-3">
+                <p className="font-bold text-sm">{username}</p>
+                <p className="text-sm">{fullName}</p>
+            </div>
         </Link>
     )
 
     export default User;
 
 User.propTypes = {
-    username: PropTypes.string,
-    fullName: PropTypes.string
+    username: PropTypes.string.isRequired,
+    fullName: PropTypes.string.isRequired
 }
