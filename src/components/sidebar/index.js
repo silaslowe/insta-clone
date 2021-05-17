39572bf45c3c034cfd20465c01c1f9e4b9270b99
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-05-17 11:46:51 -0500

    added react skeleton to dependencies and started the user component

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -3,7 +3,9 @@ import User from './user'
 import Suggestions from './suggestions'
 
 export default function Sidebar() {
-    const { user: {fullName, username, userId} } = useUser()
+    const { 
+        user: {fullName, username, userId} 
+    } = useUser()
        return <div className="p-4">
            <User username={username} fullName={fullName}/>
            <Suggestions userId={userId}/>
--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -1,3 +1,20 @@
-export default function User() {
-    return <p>I am a user</p>
+import { memo } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import Skeleton from 'react-loading-skeleton'
+
+const User = ({username, fullName}) =>
+    !username || !fullName ? (
+        <Skeleton count={1} height={61} />
+    ) : (
+        <Link to={`/p/${username}`} className="grid">
+            <p>{username}</p>
+        </Link>
+    )
+
+    export default User;
+
+User.propTypes = {
+    username: PropTypes.string,
+    fullName: PropTypes.string
 }
