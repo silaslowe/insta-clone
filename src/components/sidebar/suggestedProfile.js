a63f6fc18f744e64079b44133d6fec5d4a50a792
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-05-21 08:43:32 -0500

    suggested profiles update and rerender

--- src/components/sidebar/suggestedProfile.js
+++ src/components/sidebar/suggestedProfile.js
@@ -1,25 +1,33 @@
 import PropTypes from 'prop-types'
 import {useState} from 'react'
 import { Link } from 'react-router-dom'
+import {updateLoggedInUserFollowing, updateFollowedUserFollowers} from '../../services/firebase'
 
-export default function SuggestedProfile({profileDocId, username, profileId, userId}) {
+export default function SuggestedProfile({profileDocId, username, profileId, userId, loggedInUserDocId }) {
     const [followed, setFollowed] = useState(false)
-    
+
+    async function handleFollowUsers() {
+        setFollowed(true)
+
+        await updateLoggedInUserFollowing(loggedInUserDocId, profileId, false)
+
+        await updateFollowedUserFollowers(profileDocId, userId, false)
+    }
+
     return !followed ? (
-        <div className=" flex flex-row items-center align-items justify-between">
+        <div className="flex flex-row items-center align-items justify-between">
             <div className="flex items-center justify-between">
                 <img className="rounded-full w-8 flex mr-3" src={`/images/avatars/${username}.jpg`} alt="" />
                 <Link to={`/p/${username}`}>
                     <p className="font-bold text-sm">{username}</p>
                 </Link>
-                <div>
+                    </div>
+
                 <button
                     className="text-xs font-bold text-blue-medium"
                     type="button"
-                    onClick={() => console.log('Follow this Link')}
+                    onClick={handleFollowUsers}
                 >Follow</button>
-            </div>
-            </div>
         </div>
     ) :
     null
@@ -29,5 +37,6 @@ SuggestedProfile.propTypes = {
     profileDocId: PropTypes.string.isRequired,
     username: PropTypes.string.isRequired,
     profileId: PropTypes.string.isRequired,
-    userId: PropTypes.string.isRequired
+    userId: PropTypes.string.isRequired,
+    loggedInUserDocId: PropTypes.string.isRequired
 }
